789439ceed8cb7bbe9260884600d29287149991d
ibrahim-soumare <ibrahim.soumare@laplateforme.io>
2023-11-23 11:36:30 +0100

    job07

---
+++ jour03/job07/main.py
@@ -0,0 +1,15 @@
+def developpeur(langage):
+    if langage == "JavaScript":
+        print("Tu es un développeur web.")
+    elif langage == "Python":
+        print("Tu es un développeur IA.")
+    elif langage == "Java":
+        print("Tu es un développeur logiciel.")
+    elif langage == "ReactJS":
+        print("Tu es un développeur mobile.")
+    else:
+        print("Un jour, je serai le meilleur développeur...")
+
+
+developpeur("aaaa")
+
