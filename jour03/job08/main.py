b4baa4b26fc37aed9064dfb4dbd6919684dd141c
ibrahim-soumare <ibrahim.soumare@laplateforme.io>
2023-11-23 11:46:55 +0100

    job09

---
+++ jour03/job08/main.py
@@ -0,0 +1,13 @@
+def afficher_produits(type, saison):
+    if type == "fruits" and saison == "hiver":
+        print("Orange, mandarine et kiwi")
+    elif type == "fruits" and saison == "ete":
+        print("Poire, fraise, cassis")
+    elif type == "legume" and saison == "hiver":
+        print("Carotte, topinambour, endive")
+    elif type == "legume" and saison == "ete":
+        print("Artichaut, aubergine, navet")
+    else:
+        print("Aucune correspondance trouvée.")
+
+
